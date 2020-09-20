3c357a895d04dfac10558f6e53d44c9aef308000
András Maróy <andras@maroy.hu>
2020-09-20 18:06:32 +0200

    Move application definitions from Brewfile to ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,6 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
   fi
 fi
 
-readonly GIT_DIR="$(git rev-parse --show-toplevel | sed -e "s:${HOME}/::")"
-
 # Check submodules
 if git submodule status | grep '^-' &> /dev/null; then
   echo 'Submodules uninitialized, updating...'
@@ -21,57 +19,3 @@ fi
 pip3 install --user pipenv
 pipenv install ansible
 pipenv run ansible-playbook --connection=local --inventory 127.0.0.1, --ask-become-pass site.yml
-
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-  if ! brew tap | grep -q Homebrew/bundle &> /dev/null; then
-    echo 'Setting up Homebrew Bundle'
-    brew tap Homebrew/bundle
-  fi
-  echo 'Installing packages from Brewfile'
-  brew bundle -f Brewfile
-  brew bundle -f Brewfile-work
-  if brew list fzf &> /dev/null; then
-    /usr/local/opt/fzf/install
-  fi
-  if command -v vagrant &> /dev/null; then
-    if ! vagrant plugin list | grep -qs 'vagrant-vmware-fusion'; then
-      vagrant plugin install vagrant-vmware-fusion
-      echo 'Vagrant vmware fusion provider installed, setup the license with:'
-      echo 'vagrant plugin license vagrant-vmware-fusion ~/license.lic'
-    fi
-  fi
-  brew cleanup
-  brew cask cleanup
-fi
-
-echo 'Creating necessary folders in ~'
-mkdir -p ~/.config
-mkdir -p ~/.go
-
-echo 'Creating symlinks'
-ln -isv "../${GIT_DIR}/config/flake8" ~/.config/flake8
-ln -isv "${GIT_DIR}/jshintrc" ~/.jshintrc
-
-echo 'Installing python packages'
-export PATH="/usr/local/opt/python/libexec/bin:$PATH"
-pip install -r requirements.txt
-# gem install puppet-lint
-
-if command -v bat &> /dev/null; then
-  echo 'Setting up bat theme'
-  BAT_CONFIG_DIR="$(bat --config-dir)"
-  mkdir -p "$BAT_CONFIG_DIR/themes"
-  pushd "$BAT_CONFIG_DIR/themes" > /dev/null
-  wget https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/textmate/Tomorrow-Night-Eighties.tmTheme
-  popd > /dev/null
-
-  mkdir -p "$BAT_CONFIG_DIR/syntaxes"
-  pushd "$BAT_CONFIG_DIR/syntaxes" > /dev/null
-  wget https://raw.githubusercontent.com/aziz/PlainTasks/master/PlainTasks.sublime-syntax
-  wget https://raw.githubusercontent.com/alexlouden/Terraform.tmLanguage/master/Terraform.sublime-syntax
-  # shellcheck disable=SC1003
-  sed -e 's/\(file_extensions:$\)/\1\'$'\n  - TODO/' PlainTasks.sublime-syntax
-  popd > /dev/null
-
-  bat cache --build
-fi
