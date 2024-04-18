ece048db2e67e5ab84a55abdac374ce63af1b107
András Maróy <andras@maroy.hu>
2024-04-18 17:01:30 +0200

    Add pwd to fish prompt

--- roles/fish/files/functions/fish_prompt.fish
+++ roles/fish/files/functions/fish_prompt.fish
@@ -1,3 +1,6 @@
 function fish_prompt
     echo -n "$(set_color --bold white)$_prompt_time $(set_color normal)"
+
+    echo "$(set_color --bold white)$_prompt_pwd$(set_color normal)"
+    echo -n "\$ "
 end
