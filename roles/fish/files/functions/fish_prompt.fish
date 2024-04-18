fd9f06c0546a22d670a5147c9eccc09bb87032ae
András Maróy <andras@maroy.hu>
2024-04-18 17:11:54 +0200

    Add error exit codes to fish prompt

--- roles/fish/files/functions/fish_prompt.fish
+++ roles/fish/files/functions/fish_prompt.fish
@@ -1,5 +1,11 @@
 function fish_prompt
+    set --local exit_code $status  # save previous exit code
+
     echo -n "$(set_color --bold white)$_prompt_time $(set_color normal)"
+
+    if test $exit_code -ne 0 -a $exit_code -ne 146
+        echo -n "[$(set_color red)$exit_code$(set_color normal)] "
+    end
 
     echo "$(set_color --bold white)$_prompt_pwd$(set_color normal)"
     echo -n "\$ "
