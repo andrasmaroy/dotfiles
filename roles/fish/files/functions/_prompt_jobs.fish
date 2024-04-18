3caa44894928f4fa4cfb7bc10444160b25a26534
András Maróy <andras@maroy.hu>
2024-04-18 18:45:44 +0200

    Separate stopped and running jobs in fish prompt

--- roles/fish/files/functions/_prompt_jobs.fish
+++ roles/fish/files/functions/_prompt_jobs.fish
@@ -1,8 +1,10 @@
 function _prompt_jobs
-    set --local njobs (count (jobs -p))
-    if test "$njobs" -gt 0
+    set --local stopped_jobs (count (jobs | awk '{print $3}' | grep stopped))
+    set --local running_jobs (count (jobs | awk '{print $3}' | grep running))
+
+    if test $stopped_jobs -gt 0 -o $running_jobs -gt 0
         set_color --bold white
-        echo -n "[$njobs] "
+        printf '[%sr/%ss] ' $running_jobs $stopped_jobs
         set_color normal
     end
 end
