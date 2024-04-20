2d3c85ef550202bdbb08d80518ecd53220c6a7ba
András Maróy <andras@maroy.hu>
2024-04-20 11:21:21 +0200

    Set fish async prompt functions instead of whole prompt

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -3,4 +3,5 @@ if status is-interactive
     set fzf_diff_highlighter delta --paging=never --width=20
 
     set -U async_prompt_inherit_variables status pipestatus SHLVL CMD_DURATION fish_bind_mode VIRTUAL_ENV
+    set -U async_prompt_functions fish_right_prompt _prompt_git _prompt_hostname _prompt_pwd _prompt_user _prompt_virtualenv
 end
